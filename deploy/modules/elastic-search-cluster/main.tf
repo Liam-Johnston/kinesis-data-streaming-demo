c3b1528bdaa83073ed818baacf4927057ee4396a
Liam Johnston <liam.johnston@contino.io>
2021-12-17 15:52:47 +1100

    Added password min spec

--- deploy/modules/elastic-search-cluster/main.tf
+++ deploy/modules/elastic-search-cluster/main.tf
@@ -2,10 +2,10 @@ resource "random_password" "password" {
   length  = 16
   special = true
 
-  # min_upper   = 1
-  # min_lower   = 1
-  # min_numeric = 1
-  # min_special = 1
+  min_upper   = 1
+  min_lower   = 1
+  min_numeric = 1
+  min_special = 1
 }
 
 resource "aws_elasticsearch_domain" "this" {
